ef6c9edf438cd553cacc5e47670856370a8a1736
Karan Gupta <gkaran184@gmail.com>
2019-04-14 17:07:51 +0530

    Added Testing notebook and graph. Updated model
    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- production.py
+++ production.py
@@ -102,7 +102,7 @@ input_full = np.minimum(input_crop, 1.0)
 
 # Restore trained model
 saver = tf.train.Saver()
-saver.restore(sess, "models/my-test-model4.ckpt")
+saver.restore(sess, "models/my-test-model6.ckpt")
 
 # Run forward pass to get output
 output = sess.run([output_image], feed_dict={ input_image: input_full})
