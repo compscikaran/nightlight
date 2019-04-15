8020cfca0a7ed2b3775eb6d5488e23bb284da801
Karan Gupta <gkaran184@gmail.com>
2019-04-15 12:13:53 +0530

    Used postprocess in place of demosaicing
    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- converter.py
+++ converter.py
@@ -8,12 +8,13 @@ OETF = colour.OETFS['sRGB']
 
 def bayer_to_rgba(filename):
     im = rawpy.imread(filename)
-    image = im.raw_image_visible.astype(np.float32)
-    x = demosaicing_CFA_Bayer_bilinear(image)
-    r_channel, g_channel, b_channel = cv2.split(x)
-    alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype)
-    img_RGBA = cv2.merge((r_channel, g_channel, b_channel, alpha_channel))
-    y = [img_RGBA/np.max(img_RGBA)]
-    return np.array(y)
-    
+    rgb = im.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+    rgb = rgb / np.max(rgb)
+    r, g, b = cv2.split(rgb)
+    alpha = np.ones(r.shape, dtype=r.dtype)
+    rgba = cv2.merge((r, g, b, alpha))
+    rgba = rgba/np.max(rgba)
+    # rgba = rgba * 255
+    y = [rgba]
+    return np.array(y)    
 
